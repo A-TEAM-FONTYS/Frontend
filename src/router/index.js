3b749af7992db69d23b5d20540938fe8b482b5b2
Breg,Coen C.J <c.breg@student.fontys.nl>
2020-05-29 13:18:43 +0200

    init page, added routing

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  }
+  },
+  {
+    path: "/stats",
+    name: "Stats",
+    component: () => import("../views/Stats/index.vue")
+  },
 ]
 
 const router = new VueRouter({
