ade11776532e5d4041ff268ed819fba75a430be7
Breg,Coen C.J <c.breg@student.fontys.nl>
2020-05-29 11:07:34 +0200

    added icon component

---
+++ src/components/base/NavBarComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar text-limed-sprouce flex items-center fixed bottom-0 left-0 w-full">
+    <div class="container mx-auto px-8 flex justify-between">
+      <router-link :to="{ name: 'entries.index' }" class="opacity-50">
+        <Icon name="clock" />
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
