1bcb33378044ab30ae9351e7580f086a4401928e
Martin Peeters <406336@student.fontys.nl>
2020-06-04 11:29:28 +0200

    Changed font of Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-outer-space h-screen w-screen p-6 text-white">
+  <div class="font-serif bg-outer-space h-screen w-screen p-6 text-white">
     <div class="grid grid-rows-3 grid-flow-col gap-10">
       <div class="row-span-2">
         <div class="h-full flex items-center">
@@ -32,7 +32,11 @@
     <div class="flex w-full h-24 mt-6 overflow-hidden">
       <div class="flex-1 h-full pr-3">
         <div class="h-full bg-limed-spruce rounded-md p-3">
-          <h4 class="text-green-mist text-xs uppercase tracking-widest font-semibold">Day Streak</h4>
+          <h4
+            class="text-green-mist text-xs uppercase tracking-widest font-semibold"
+          >
+            Day Streak
+          </h4>
           <div class="mt-2">
             <div class="flex items-end h-16 table-cell">
               <span class="text-4xl text-form-green leading-10">12</span>
@@ -43,11 +47,17 @@
       </div>
       <div class="flex-1 h-full pl-3">
         <div class="h-full bg-limed-spruce rounded-md p-3">
-          <h4 class="text-green-mist text-xs uppercase tracking-widest font-semibold">Sessions</h4>
+          <h4
+            class="text-green-mist text-xs uppercase tracking-widest font-semibold"
+          >
+            Sessions
+          </h4>
           <div class="mt-2">
             <div class="flex items-end h-16 table-cell">
               <span class="text-4xl leading-10">6</span>
-              <span class="text-xs ml-2 leading-10 tracking-wide">Completed</span>
+              <span class="text-xs ml-2 leading-10 tracking-wide"
+                >Completed</span
+              >
             </div>
           </div>
         </div>
@@ -61,5 +71,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
