94e34634c0028b7a1bfaf807049c1c375ab1b3e8
Harun-Cyber <57184978+Harun-Cyber@users.noreply.github.com>
2020-06-04 14:51:16 +0200

    Added questions component in router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app" class="bg-gray-200 w-full h-screen bg-limed-spruce">
-    <NavBar />
+    <router-view />
+    <!-- <NavBar /> -->
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/base/NavBarComponent'
+// import NavBar from '@/components/base/NavBarComponent'
 
 export default {
-  components: {
-    NavBar
-  }
+//   components: {
+//     NavBar
+//   }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Questions from '../views/questions/index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'questions',
+    component: Questions
   }
 ]
 
