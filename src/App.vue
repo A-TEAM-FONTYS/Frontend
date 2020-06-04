df166affa90fcef96c9426682f6bf6ebcde6ad22
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-04 11:22:55 +0200

    Added dynamic bg color and navbar based on route

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <div id="app" class="w-full h-screen bg-limed-spruce">
-    <router-view />
+  <div
+    id="app"
+    class="w-full h-screen"
+    :class="
+      this.$route.path == '/login' || this.$route.path == '/register'
+        ? 'bg-pampas'
+        : 'bg-limed-spruce'
+    "
+  >
+    <transition :name="transitionName" mode="out-in">
+      <router-view />
+    </transition>
     <NavBar
       v-if="this.$route.path != '/login' && this.$route.path != '/register'"
     />
@@ -10,11 +20,50 @@
 <script>
 import NavBar from '@/components/base/NavBarComponent'
 
+const DEFAULT_TRANSITION = 'fade'
+
 export default {
+  data: () => ({
+    transitionName: DEFAULT_TRANSITION
+  }),
   components: {
     NavBar
+  },
+  watch: {
+    $route(to, from) {
+      let transitionName = to.meta.transitionName || from.meta.transitionName
+
+      if (transitionName === 'slide') {
+        const toPath = to.path
+        transitionName = toPath === '/register' ? 'slide-right' : 'slide-left'
+      }
+
+      this.transitionName = transitionName || DEFAULT_TRANSITION
+    }
   }
 }
 </script>
 
-<style lang="postcss"></style>
+<style lang="postcss">
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition-duration: 0.5s;
+  transition-property: height, opacity, transform;
+  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+  overflow: hidden;
+}
+
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate(2em, 0);
+}
+
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: 0;
+  transform: translate(-2em, 0);
+}
+</style>
