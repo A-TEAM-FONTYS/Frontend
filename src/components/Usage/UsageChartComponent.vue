d5c93858be8630f38738379f9b7d86bce0135ac4
Breg,Coen C.J <c.breg@student.fontys.nl>
2020-06-04 10:06:33 +0200

    Fake date added to chart

---
+++ src/components/Usage/UsageChartComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card flex justify-center">
+    <div class="m-8 flex flex-col justify-center items-center text-white rounded-full relative">
+      <DoughnutChart :height="180" :width="180" />
+    </div>
+  </div>
+</template>
+
+<script>
+import DoughnutChart from '@/components/Usage/DoughnutChartComponent'
+
+export default {
+  components: {
+    DoughnutChart
+  }
+}
+</script>
--- src/views/Stats/Index.vue
+++ src/views/Stats/Index.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="py-10 px-6 text-pampas font-serif">
     <h1 class="text-2xl">App usage</h1>
-    <div class="flex justify-center">
-      <Picker @updatePicker="test" class="mt-4" />
+    <div class="flex flex-col">
+      <div class="flex justify-center">
+        <Picker @updatePicker="test" class="mt-4" />
+      </div>
+      <div class="flex justify-center">
+        <Chart />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Picker from '@/components/stats/PickerComponent'
+import Chart from '@/components/Usage/UsageChartComponent'
+
 export default {
   components: {
-    Picker
+    Picker,
+    Chart
   },
   methods: {
     test(active) {
