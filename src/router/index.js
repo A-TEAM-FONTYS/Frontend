b79c7ee5dba4ac619a8eb3139e6cbc59b5aa7714
Harun-Cyber <57184978+Harun-Cyber@users.noreply.github.com>
2020-06-05 11:02:42 +0200

    Added png and changed unnecesary things

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// import Home from '../views/Home.vue'
 import Questions from '../views/questions/index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
   {
-    path: '/',
+    path: '/views',
     name: 'questions',
     component: Questions
   }
