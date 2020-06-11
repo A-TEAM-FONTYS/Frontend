cfc2b1a72a619ac7f08afc3abad9dbcc71582801
Breg,Coen C.J <c.breg@student.fontys.nl>
2020-06-11 11:36:25 +0200

    Removed comments

--- src/components/Usage/DoughnutChartComponent.vue
+++ src/components/Usage/DoughnutChartComponent.vue
@@ -24,7 +24,6 @@ export default {
   created() {
     this.chartData.datasets = this.datasets
     this.chartData.labels = this.labels
-    //this.renderChart(this.chartData, this.chartOptions)
   },
   mounted() {
     this.renderChart(this.chartData, this.chartOptions)
@@ -41,8 +40,13 @@ export default {
             '#969E88',
             '#778475',
             '#576961',
+            '#475B58',
+            '#B6B99C',
+            '#969E88',
+            '#778475',
+            '#576961',
             '#475B58'
-          ], //give color list
+          ],
           borderColor: '#374E4E',
           borderWidth: 0
         }
