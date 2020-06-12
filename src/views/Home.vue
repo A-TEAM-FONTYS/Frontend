6a20581dfafb45885d7bb32f971651c08cca2c69
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-12 13:08:55 +0200

    Refactored home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,29 @@
   <div
     class="font-serif bg-outer-space h-screen w-screen p-6 text-pampas w-full sm:w-2/3 sm:mx-auto"
   >
-    <div class="grid grid-rows-3 grid-flow-col gap-10">
-      <div class="row-span-2">
-        <div class="h-full flex items-center">
-          <div>
-            <span class="block w-full text-2xl">Welcome back,</span>
-            <span class="font-medium text-2xl">{{ user.firstName }}</span>
-          </div>
-        </div>
+    <div class="flex justify-between items-center mb-6">
+      <div class="flex flex-col">
+        <span class="block w-full text-2xl">Welcome,</span>
+        <span class="font-medium text-2xl">{{ user.firstName }}</span>
       </div>
-      <div class="row-span-2">
-        <div class="flex justify-end">
-          <img src="@/assets/treeArt.png" class="sm:w-24 sm:h-24" />
-        </div>
+      <img src="@/assets/treeArt.png" class="w-32" />
+    </div>
+    <div class="bg-limed-spruce rounded-md p-3 overflow-hidden mb-20">
+      <div class="relative z-10">
+        <img
+          src="@/assets/dotsArt.png"
+          class="absolute -bottom-8 -right-24 transform rotate-90 -z-1"
+        />
+        <a href="/quiz">
+          <div class="flex flex-col p-2">
+            <div class="text-sm mb-2 text-green-mist">Setup</div>
+            <h3 class="text-2xl">Start the intro quiz!</h3>
+          </div>
+        </a>
       </div>
     </div>
-    <div class="w-full -mt-16 overflow-hidden">
+
+    <div class="-mt-16">
       <div class="flex justify-center">
         <div
           class="bg-pampas w-20 h-20 rounded-full border-8 border-outer-space"
@@ -31,27 +38,29 @@
         </div>
       </div>
       <div
-        class="-mt-8 w-full pb-4 bg-limed-spruce rounded-md text-center overflow-hidden"
+        class="-mt-10 pb-4 bg-limed-spruce rounded-md text-center overflow-hidden"
       >
         <div class="relative z-10">
-          <img
-            src="@/assets/circlesArt.png"
-            class="absolute -top-8 -left-8 transform rotate-45 -z-1"
-          />
-          <img
-            src="@/assets/circlesArt.png"
-            class="absolute -bottom-16 -right-16 transform rotate-45 -z-1"
-          />
-          <div class="inline-block align-bottom">
-            <h3 class="text-2xl mt-12">Continue your daily quiz</h3>
-            <p class="text-sm">Become aware of your app usage</p>
-          </div>
+          <a href="/quiz">
+            <img
+              src="@/assets/circlesArt.png"
+              class="absolute -top-8 -left-8 transform rotate-45 -z-1"
+            />
+            <img
+              src="@/assets/circlesArt.png"
+              class="absolute -bottom-16 -right-16 transform rotate-45 -z-1"
+            />
+            <div class="inline-block align-bottom">
+              <h3 class="text-2xl mt-12">Continue your daily quiz</h3>
+              <p class="text-sm">Become aware of your app usage</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
-    <div class="flex w-full h-24 mt-6 overflow-hidden">
-      <div class="flex-1 h-full pr-3">
-        <div class="h-full bg-limed-spruce rounded-md p-3 overflow-hidden">
+    <div class="flex justify-between mt-6">
+      <div class="w-1/2 mr-6">
+        <div class="bg-limed-spruce rounded-md p-3 overflow-hidden">
           <div class="relative z-10">
             <img
               src="@/assets/linesArt.png"
@@ -61,16 +70,16 @@
               Day Streak
             </h4>
             <div class="mt-2">
-              <div class="flex items-end h-16 table-cell">
-                <span class="text-4xl text-form-green leading-10">12</span>
+              <div class="flex items-end">
+                <span class="text-4xl text-form-green leading-10">0</span>
                 <span class="text-xs ml-2 leading-10 tracking-wide">days</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="flex-1 h-full pl-3">
-        <div class="h-full bg-limed-spruce rounded-md p-3 overflow-hidden">
+      <div class="w-1/2">
+        <div class="bg-limed-spruce rounded-md p-3 overflow-hidden">
           <div class="relative z-10">
             <img
               src="@/assets/dotsArt.png"
@@ -80,8 +89,8 @@
               Sessions
             </h4>
             <div class="mt-2">
-              <div class="flex items-end h-16 table-cell">
-                <span class="text-4xl leading-10">6</span>
+              <div class="flex items-end">
+                <span class="text-4xl leading-10">0</span>
                 <span class="text-xs ml-2 leading-10 tracking-wide"
                   >completed</span
                 >
