b0168afaa6b2827d6699e1188fe856f36c35e880
Efaliso <efalisotamerat@gmail.com>
2020-06-12 14:58:15 +0200

    new ui implemented

--- src/views/Exercises/AppAdvantages.vue
+++ src/views/Exercises/AppAdvantages.vue
@@ -1,54 +1,49 @@
 <template>
   <div class="w-full h-full p-5 bg-outer-space justify-center">
-    <div class="text-white text-base m-5">
-      <div class>
-        Preparation Question B
+    <div class="text-white text-base m-4 mb-12">
+      <div class="text-xl">
+        Question 15
         <a class="float-right text-sunglow">X</a>
       </div>
     </div>
-    <div class="p-5 mt-3 ml-1">
-      <div class="w-fit-content text-black rounded-full p-5 mx-auto bg-white shadow-xl">
-        <Icon name="instagram" />
-      </div>
-      <div class="text-lg text-white text-center mt-1">Short term - Advantages</div>
-      <form @submit.prevent="submit">
-        <div class="p-5 mt-5 left-0 bottom-0 right-0 top-0 mx-auto">
-          <ul class="text-xl text-white">
-            <li class="pb-5" v-for="item in suggestions" :key="item.suggestion">
-              <label class="block text-sm block">
-                <input
-                  @click="item.checked ? unselectSuggestion(item) : selectSuggestion(item)"
-                  class="w-6 h-6 align-bottom relative mr-3"
-                  type="checkbox"
-                />
-                {{ item.suggestion }}
-              </label>
-            </li>
-            <li>
-              <div class="flex">
-                <a class="cursor-pointer" @click="addCustomSuggestion(customSuggestion)">
-                  <img class="bg-white p-1 w-6 max-w-xs mr-3" src="@/assets/plus.png" />
-                </a>
-                <input
-                  v-model="customSuggestion.suggestion"
-                  class="ml-1 text-white bg-transparent text-sm w-full"
-                  type="text"
-                  style="border-bottom: 1px solid #fff;"
-                />
-              </div>
-            </li>
-          </ul>
-        </div>
-      </form>
+    <div class="w-fit-content text-black rounded-full p-3 mx-auto mb-4 bg-white shadow-xl">
+      <Icon name="facebook" width="30px" height="30px" />
+    </div>
+    <div class="mb-12">
+      <div class="text-lg text-white text-center mt-2">Facebook - Short Term</div>
+      <div class="text-lg text-white text-center">Advantages</div>
+    </div>
+    <div class="p-5 mt-5">
+      <ul class="text-xl text-white">
+        <li class="pb-3" v-for="item in suggestions" :key="item.suggestion">
+          <div
+            :class="item.checked ? 'bg-kindagold' : 'bg-transparent'"
+            @click="item.checked ? unselectSuggestion(item) : selectSuggestion(item)"
+            class="text-sm border-solid border border-white rounded-lg p-1"
+          >
+            <div class="text-center">{{item.suggestion}}</div>
+          </div>
+        </li>
+        <li>
+          <div class="flex">
+            <input
+              v-model="customSuggestion.suggestion"
+              class="text-white bg-kindagreen text-sm w-full p-1 border-solid border border-white rounded-lg"
+              type="text"
+              placeholder=" Add your own advantage..."
+            />
+          </div>
+        </li>
+      </ul>
     </div>
     <div class="left-0 bottom-0 w-full fixed p-6 bg-outer-space">
       <div class="button-group">
         <button
-          class="w-24 p-1 float-left bg-limed-spruce text-sunglow rounded-md"
+          class="w-24 p-2 float-left bg-limed-spruce text-kindagold rounded-md"
           type="back"
-        >Previous</button>
+        >Back</button>
         <button
-          class="w-24 p-1 float-right bg-sunglow text-limed-spruce rounded-md"
+          class="w-24 p-2 float-right bg-kindagold text-limed-spruce rounded-md"
           type="submit"
           @click="saveInDatabase()"
         >Next</button>
@@ -74,10 +69,10 @@ export default {
       },
       suggestion: { id: '', suggestion: '', checked: '' },
       suggestions: [
-        {  suggestion: 'Enjoy using it', checked: false },
-        {  suggestion: 'I am whoever I want to be', checked: false },
-        {  suggestion: 'Chat with friends', checked: false },
-        { suggestion: 'Staying up to date with events', checked: false }
+        { suggestion: 'I enjoy using it.', checked: false },
+        { suggestion: 'I can be whoever I want to be.', checked: false },
+        { suggestion: 'Chatting with friends', checked: false },
+        { suggestion: 'Staying up to date with events.', checked: false }
       ],
       checkedSuggestions: []
     }
@@ -92,16 +87,19 @@ export default {
     },
     unselectSuggestion(suggestion) {
       this.checkedSuggestions.splice(suggestion, 1)
-      suggestion.checked = false;
+      suggestion.checked = false
       console.log(this.checkedSuggestions)
     },
     addCustomSuggestion(customSuggestion) {
       this.suggestions.push({ suggestion: customSuggestion.suggestion })
-      this.customSuggestion.suggestion = '';
+      this.customSuggestion.suggestion = ''
       console.log(customSuggestion)
     },
-    saveInDatabase(){
-      console.log('not implemented');
+    saveInDatabase() {
+      console.log('not implemented')
+    },
+    test(item) {
+      console.log(item.suggestion)
     }
   },
   components: {
