6a054a4a8edddbeb57029c0f525a25aaeb707b18
Harun-Cyber <57184978+Harun-Cyber@users.noreply.github.com>
2020-06-18 13:02:20 +0200

    added footercomponent and buttoncomponent

--- src/views/questions/quetion1.vue
+++ src/views/questions/quetion1.vue
@@ -16,38 +16,38 @@
         </div>
       </div>
     <div class="mt-5 flex">
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">1</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">2</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">3</button>
+        <v-button name="Snapchat"></v-button>
+        <v-button name="Facebook"></v-button>
+        <v-button name="Twitter"></v-button>
     </div>
     <div class="mt-1 flex">
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">1</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">2</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">3</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">4</button>
+        <v-button name="Reddit"></v-button>
+        <v-button name="Netflix"></v-button>
+        <v-button name="Discord"></v-button>
+        <v-button name="Tiktok"></v-button>
     </div>
     <div class="mt-1 flex">
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">1</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">2</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">3</button>
+       <v-button name="10"></v-button>
+       <v-button name="10"></v-button>
+       <v-button name="10"></v-button>
     </div>
     <div class="mt-1 flex">
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">1</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">2</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">3</button>
-        <button class="flex-1 font-serif text-pampas-700 text-center border-2 border-pampas px-4 py-2 m-2 rounded-lg">3</button>
+        <v-button name="10"></v-button>
+        <v-button name="10"></v-button>
+        <v-button name="10"></v-button>
+        <v-button name="10"></v-button>
     </div>
-    <footer class="left-0 bottom-0 w-full fixed p-6 bg-outer-space">
-      <div class="button-group">
-        <button class="w-24 p-1 font-serif float-left bg-limed-spruce text-pampas rounded-md">Cancel</button>
-        <button class="w-24 p-1 font-serif float-right bg-pampas text-limed-spruce rounded-md">Next</button>
-      </div>
-    </footer>
+    <v-footer></v-footer>
 </div>
 </template>
 <script>
+    import Button from '@/components/Questions/buttonComponent.vue'
+    import Footer from '@/components/Questions/footer.vue'
 
-export default {
-  name: 'introquiz1'
+    export default {
+    components:{
+      'v-button' : Button,
+      'v-footer' : Footer
+    }
 }
 </script>
