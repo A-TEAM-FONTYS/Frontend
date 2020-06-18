0453be07bb0f9a4a20be520df6d546a5f26900f3
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-18 10:19:38 +0200

    created setup questions component

--- src/components/questions/SetupQuestionComponent.vue
+++ src/components/questions/SetupQuestionComponent.vue
@@ -15,59 +15,70 @@
         <span class="text-2xl">Application Name</span>
       </div>
     </div>
-    <div>
-      <div>
-        <span>At what age did you start using this app?</span>
-        <input
-          type="number"
-          name=""
-          id=""
-          min="0"
-          max="99"
-          class="bg-outer-space border border-green-mist rounded-lg text-center px-4 py-2 w-16"
-        />
+    <div class="flex flex-col justify-center text-center">
+      <div class="flex flex-col my-4">
+        <span class="mb-2">At what age did you start using this app?</span>
+        <div class="flex justify-center">
+          <input
+            type="number"
+            name=""
+            id=""
+            min="0"
+            max="99"
+            class="bg-outer-space border border-green-mist rounded-lg text-center px-4 py-2 w-16"
+          />
+        </div>
       </div>
-      <div>
-        <span>How many times a day are you using this app?</span>
-        <input
-          type="number"
-          name=""
-          id=""
-          min="0"
-          max="99"
-          class="bg-outer-space border border-green-mist rounded-lg text-center px-4 py-2 w-16"
-        />
+      <div class="flex flex-col my-4">
+        <span class="mb-2">How many times a day are you using this app?</span>
+        <div class="flex justify-center">
+          <input
+            type="number"
+            name=""
+            id=""
+            min="0"
+            max="99"
+            class="bg-outer-space border border-green-mist rounded-lg text-center px-4 py-2 w-16"
+          />
+        </div>
       </div>
-      <div>
-        <span>
+      <div class="flex flex-col my-4">
+        <span class="mb-2">
           How much do you need this app
           <br />
 
           (0 nothing at all, 5 very essential)
         </span>
-        <input
-          type="range"
-          name=""
-          id=""
-          min="1"
-          max="5"
-          class="bg-outer-space border border-green-mist rounded-lg text-center px-4 py-2 w-16"
-        />
+        <div class="flex justify-center">
+          <input
+            type="range"
+            name=""
+            id=""
+            min="1"
+            max="5"
+            value="3"
+            class="bg-outer-space border border-green-mist rounded-lg text-center "
+          />
+        </div>
       </div>
-      <div>
-        <span>
-          To what extent do you experience it as a problem?
+      <div class="flex flex-col my-4">
+        <span class="mb-2">
+          To what extent do you experience <br />
+          it as a problem?
           <br />
           (0 nothing at all, 5 very essential)
         </span>
-        <input
-          type="range"
-          name=""
-          id=""
-          min="1"
-          max="5"
-          class="bg-outer-space border border-green-mist rounded-lg text-center px-4 py-2 w-16"
-        />>
+        <div class="flex justify-center">
+          <input
+            type="range"
+            name=""
+            id=""
+            min="1"
+            max="5"
+            value="3"
+            class="bg-outer-space border border-green-mist rounded-lg text-center"
+          />
+        </div>
       </div>
     </div>
   </div>
