bf4306a77ecb8d8875da7163ce0cb6a7691b9775
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-18 11:38:04 +0200

    Refactored donut component

--- src/components/Usage/DoughnutChartComponent.vue
+++ src/components/Usage/DoughnutChartComponent.vue
@@ -1,18 +1,11 @@
-
 <script>
 import { Doughnut } from 'vue-chartjs'
 
-import { mapGetters } from 'vuex'
-
 export default {
   extends: Doughnut,
-  props: {},
+  props: ['chartData'],
   data() {
     return {
-      chartData: {
-        datasets: [],
-        labels: []
-      },
       chartOptions: {
         legend: {
           display: false
@@ -21,41 +14,8 @@ export default {
       }
     }
   },
-  created() {
-    this.chartData.datasets = this.datasets
-    this.chartData.labels = this.labels
-  },
   mounted() {
     this.renderChart(this.chartData, this.chartOptions)
-  },
-  computed: {
-    ...mapGetters('usage', ['allAppData']),
-    datasets() {
-      return [
-        {
-          data: this.allAppData.map(i => i.timeUsed),
-          backgroundColor: [
-            '#D6D4AF',
-            '#B6B99C',
-            '#969E88',
-            '#778475',
-            '#576961',
-            '#475B58',
-            '#B6B99C',
-            '#969E88',
-            '#778475',
-            '#576961',
-            '#475B58'
-          ],
-          borderColor: '#374E4E',
-          borderWidth: 0
-        }
-      ]
-    },
-    labels() {
-      return this.allAppData.map(i => i.appName)
-    }
-  },
-  methods: {}
+  }
 }
 </script>
--- src/components/Usage/UsageChartComponent.vue
+++ src/components/Usage/UsageChartComponent.vue
@@ -1,23 +1,47 @@
 <template>
   <div class="card flex justify-center">
-    <div class="m-8 flex flex-col justify-center items-center text-white rounded-full relative">
-      <div v-if="data">
-        <DoughnutChart :height="180" :width="180" />
-      </div>
+    <div
+      class="m-8 flex flex-col justify-center items-center text-white rounded-full relative"
+    >
+      <DoughnutChart :height="180" :width="180" :chart-data="datasetsfull" />
     </div>
   </div>
 </template>
 
 <script>
 import DoughnutChart from '@/components/usage/DoughnutChartComponent'
-import { mapState } from 'vuex'
 
 export default {
+  props: ['chartData'],
   components: {
     DoughnutChart
   },
   computed: {
-    ...mapState('usage', ['data'])
+    datasetsfull() {
+      return {
+        labels: this.chartData.map(i => i.appName),
+        datasets: [
+          {
+            data: this.chartData.map(i => i.timeUsed),
+            backgroundColor: [
+              '#D6D4AF',
+              '#B6B99C',
+              '#969E88',
+              '#778475',
+              '#576961',
+              '#475B58',
+              '#B6B99C',
+              '#969E88',
+              '#778475',
+              '#576961',
+              '#475B58'
+            ],
+            borderColor: '#374E4E',
+            borderWidth: 0
+          }
+        ]
+      }
+    }
   }
 }
 </script>
