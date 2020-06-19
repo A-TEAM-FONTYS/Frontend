339ba175fd07ba17ec83f1f2d527a5621f7945fc
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-19 12:09:25 +0200

    Added quiz states

--- src/store/modules/quiz.js
+++ src/store/modules/quiz.js
@@ -3,8 +3,11 @@ import api from '@/services/quiz.js'
 export default {
   namespaced: true,
   state: {
+    quizIndex: 0,
+    quizAnswers: [],
     quiz: [],
     questions: [],
+    dayStreak: 0,
     loading: false,
     error: null
   },
@@ -14,8 +17,29 @@ export default {
     SET_LOADING(state, payload) {
       state.loading = payload
     },
+
     SET_ERROR(state, payload) {
       state.error = payload
+    },
+    RESET_STATE(state) {
+      state.quizIndex = 0
+      state.quizAnswers = null
+      state.questions = null
+      state.quiz = null
+      state.loading = false
+      state.error = null
+    },
+    increment: state => {
+      state.quizIndex++
+    },
+    decrement: state => {
+      state.quizIndex--
+    },
+    incrementDayStreak(state) {
+      state.dayStreak++
+    },
+    saveQuestionAnswer(state, payload) {
+      state.quizAnswers.push(payload)
     }
   },
   actions: {
@@ -46,11 +70,28 @@ export default {
           commit('SET_ERROR', error.response.data)
           commit('SET_LOADING', false)
         })
+    },
+    updateQuizAnswers({ commit, getters }) {
+      commit('SET_LOADING', true)
+      return api
+        .putQuizAnswers(getters.quizId, getters.quizAnswers)
+        .then(data => {
+          console.log(data)
+          commit('RESET_STATE')
+        })
+        .catch(error => {
+          commit('SET_ERROR', error.response.data)
+          commit('SET_LOADING', false)
+        })
     }
   },
   getters: {
     loading: state => state.loading,
     error: state => state.error,
-    questions: state => state.questions
+    questions: state => state.questions,
+    quizIndex: state => state.quizIndex,
+    quizId: state => state.quiz.id,
+    quizAnswers: state => state.quizAnswers,
+    dayStreak: state => state.dayStreak
   }
 }
